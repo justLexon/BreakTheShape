1e7c5ef2abf800a8eeb99f808aaf145cc5e73255
justLexon <otherslexon@gmail.com>
2025-07-06 22:42:09 -0500

    inventoryManager script

---
+++ Assets/Scripts/ShapeShopInv/InventoryManager.cs
@@ -0,0 +1,40 @@
+﻿//Manage the inventory.
+
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public static InventoryManager Instance { get; private set; }
+
+    public int maxActiveShapes = 10;
+    public List<ShapePack> shapePacks;
+
+    private HashSet<string> activeShapeIDs = new();
+
+    void Awake()
+    {
+        if (Instance != null && Instance != this) Destroy(gameObject);
+        else Instance = this;
+    }
+
+    public bool ToggleShape(string shapeID)
+    {
+        if (activeShapeIDs.Contains(shapeID))
+        {
+            activeShapeIDs.Remove(shapeID);
+            return false;
+        }
+        else if (activeShapeIDs.Count < maxActiveShapes)
+        {
+            activeShapeIDs.Add(shapeID);
+            return true;
+        }
+
+        Debug.Log("❌ Max active shapes reached");
+        return false;
+    }
+
+    public bool IsShapeActive(string shapeID) => activeShapeIDs.Contains(shapeID);
+    public int GetActiveCount() => activeShapeIDs.Count;
+}
